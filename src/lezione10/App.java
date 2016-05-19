2b740443ca0359dd91eeda3b4b7f7d8e240acff4
tss <tss@debian-tss24>
2016-05-19 21:46:58 +0200

    modifica in corso finite stampe ook

--- src/lezione10/App.java
+++ src/lezione10/App.java
@@ -5,16 +5,43 @@
  */
 package lezione10;
 
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author tss
  */
 public class App {
-    
+
     public static void main(String[] args) {
-         System.out.println("java-swing main running");
-         System.out.println("finish");
-         System.out.println("stampa di prova");
-         System.out.println("seconda prova ");
+
+        JFrame f = new JFrame("esempio swing");
+
+        f.setBounds(100, 100, 300, 200);//posizione, larghezza, altezza
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//servere per chiudere il run con la crocetta
+
+        Container contentPane = f.getContentPane(); // dichiaRO UN CONTAINER e vado a agganciare la parte di content del frame che ho fatto 
+
+        contentPane.setLayout(new FlowLayout(FlowLayout.LEFT)); //con "null" metto il posizionamento assoluto 
+                                       // flow layout posiziona a partire da sx
+        
+        
+        JButton cmd = new JButton("invia");
+        
+        cmd.setBounds(50, 50, 70, 30);
+        contentPane.add(cmd);
+        
+        JButton cmd1 = new JButton("andiamo a casa");
+        cmd1.setPreferredSize(new Dimension(100, 50));
+        
+        contentPane.add(cmd1);
+        
+        
+        f.setVisible(true);
+
     }
 }
