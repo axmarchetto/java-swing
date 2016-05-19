3e1a8183cecdeded95fde614bb2c9d795d710354
tss <tss@debian-tss24>
2016-05-19 19:51:34 +0200

    modifiche in corso

--- src/lezione10/App.java
+++ src/lezione10/App.java
@@ -14,5 +14,6 @@ public class App {
     public static void main(String[] args) {
          System.out.println("java-swing main running");
          System.out.println("finish");
+         System.out.println("stampa di prova");
     }
 }
